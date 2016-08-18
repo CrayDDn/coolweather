2be74d4ee06a149fdbe88bd9b900e41c8c916262
CrayDDn <zhoupansheng@aliyun.com>
2016-08-18 10:10:53 +0800

    切换城市刷新天气

--- app/src/main/java/com/tr/zps/app/coolweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/tr/zps/app/coolweather/activity/ChooseAreaActivity.java
@@ -77,10 +77,12 @@ public class ChooseAreaActivity extends Activity {
      */
     private int currentLevel;
 
+    private boolean isFromWeatherActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        boolean isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         if(preferences.getBoolean("city_selected",false) && !isFromWeatherActivity){
             Intent intent = new Intent(this,WeatherActivity.class);
@@ -240,6 +242,10 @@ public class ChooseAreaActivity extends Activity {
         }else if (currentLevel == LEVEL_CITY){
             queryProvinces();
         }else {
+            if(isFromWeatherActivity){
+                Intent intent = new Intent(this,WeatherActivity.class);
+                startActivity(intent);
+            }
             finish();
         }
     }
